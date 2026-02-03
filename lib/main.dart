00859330a8659fb7328e620aed858342c4a6ed0f
marinjosue <josuisaac2002@gmail.com>
2026-02-02 21:28:28 -0500

    limpio main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
 import 'core/constants/colors.dart';
